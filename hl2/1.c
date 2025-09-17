a5e60e5aeb9497ecc858c98f44ef5f8e8169f40c
Pankaj Deopa <pankajdeopa3.pd@gmail.com>
2025-09-17 11:45:47 +0000

    HL2 - 1.c

---
+++ hl2/1.c
@@ -0,0 +1,29 @@
+/*
+  ************************************************************************
+
+  Name         : template.c
+  Author       : Pankaj Deopa
+  Description  : Template File
+  Date         : 26 Aug, 2025
+
+  ************************************************************************
+*/
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main () {
+    int fd[2];
+
+	return 0;
+}
+
+
+/*
+ * **************** OUTPUT *******************
+ *
+ *
+ *
+ * 
+*/
